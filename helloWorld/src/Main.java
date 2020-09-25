78d28aa58d91fd183fdb1a64940fbc0795114a0c
Ryan Kelleher <ryan.kelleher1@mycit.ie>
2020-09-25 13:17:19 +0100

    first commit by Ryan

--- helloWorld/src/Main.java
+++ helloWorld/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World"); // -> Ronan
         //Put your code in here
+	System.out.println("Hello World"); // -> Ryan
     }
 }
